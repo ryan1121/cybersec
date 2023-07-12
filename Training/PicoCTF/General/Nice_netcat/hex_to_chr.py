cc24a4ce923baac12e06fa38859e41ae2e88a598
wubian_ <87922339+ryan1121@users.noreply.github.com>
2023-07-12 22:53:12 +0800

    Added comment in hex_to_chr.py

--- Training/PicoCTF/General/Nice_netcat/hex_to_chr.py
+++ Training/PicoCTF/General/Nice_netcat/hex_to_chr.py
@@ -1,4 +1,3 @@
-
 with open('output.txt','r') as f:
     outputList = f.readlines()
 
@@ -7,6 +6,7 @@ result = ''
 
 # Using for loop to read line by line
 for i in outputList :
+    # We are using the int() function to convert the 'i' variable which is string data type to integer data type
     result += chr(int(i))
 
 print("Result : ", result)
